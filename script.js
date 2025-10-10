74f12e0350da1c3cf1758681d3d026aed68feb89
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-10 17:02:45 -0400

    changed the folder name and added more code like a function and DOM

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
 const boardWidth = 560
-const boardHeight = 400
+const boardHeight = 300
+
+const useStart = [230, 10]
+let currentPosition = userStart
+
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
+
+// add user
+const user = document.createElement('div')
+user.classList.add('user')
+grid.appendChild(user)
+
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+
+// function
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px'
+    user.style.bottom = currentPosition[1] + 'px'
+}
