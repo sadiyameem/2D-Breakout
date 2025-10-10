ffe66276328b2601b3f71af033f4a0c991882cd0
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-10 16:52:31 -0400

    Added code in folders the start of this project

---
+++ script.js
@@ -0,0 +1,6 @@
+const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
+const blockWidth = 100
+const blockHeight = 20
+const boardWidth = 560
+const boardHeight = 400
