9ecaa2d9727a532e376baa892538bb550b8f2b63
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-12 10:53:04 -0400

    added blocks in a array

--- script.js
+++ script.js
@@ -21,6 +21,27 @@ class Block {
     }
 }
 
+// all my blocks
+const block = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+]
+
+// draw my blocks
+
 // add user
 const user = document.createElement('div')
 user.classList.add('user')
