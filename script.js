d70bc29470a9b9047167d3bb1f0be31c81e63bdd
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-12 11:22:30 -0400

    added move ball function, timerId and score

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const blockWidth = 100
 const blockHeight = 20
 const boardWidth = 560
 const boardHeight = 300
+let xDirection = -2
+let yDirection = 2
+
+let timerId
+let score = 0
 
 const userStart = [230, 10]
 let currentPosition = userStart
@@ -95,3 +100,12 @@ function moveUser() {
     }
 }
 document.addEventListener('keydown', moveUser)
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollision()
+}
+timerId = setInterval(moveBall, 30)
